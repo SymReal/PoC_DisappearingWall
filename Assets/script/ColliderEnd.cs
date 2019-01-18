453c32dce683e48a40c48b131c2cdaaa13d20aec
PosnicAntoine <antoine.posnic@gmail.com>
2019-01-18 16:40:35 +0100

    functionnal wall

---
+++ Assets/script/ColliderEnd.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderEnd : MonoBehaviour {
+
+    public GameObject player;
+
+    private Collider playerCollider;
+    private DisapearFromTop[] wallScripts;
+
+	// Use this for initialization
+	void Start () {
+        wallScripts = FindObjectsOfType<DisapearFromTop>();
+        playerCollider = player.GetComponent<Collider>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if(coll == playerCollider)
+        {
+            foreach(DisapearFromTop wall in wallScripts)
+            {
+                wall.dissolutionSpeed= 0.1F;
+            }
+        }
+    }
+}
---
+++ Assets/script/DisapearFromTop.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DisapearFromTop : MonoBehaviour {
+	
+	public float dissolutionSpeed;
+    public bool isDissolutionStart;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isDissolutionStart)
+        {
+            float curr = GetComponent<Renderer>().material.GetFloat("_Cutoff");
+            if(curr < 1)
+            {
+                GetComponent<Renderer>().material.SetFloat("_Cutoff", curr + dissolutionSpeed);
+            }
+        }
+
+    }
+}
